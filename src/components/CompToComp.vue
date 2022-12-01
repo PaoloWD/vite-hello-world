0a58700ffbe688251d7b5f607f268220b182ba1f
PaoloWD <paolo.lore.93@gmail.com>
2022-12-01 15:41:01 +0100

    test css

--- src/components/CompToComp.vue
+++ src/components/CompToComp.vue
@@ -2,6 +2,11 @@
   <div>
     <div class="text-center">
       <h1>Prendo un component da un altro component</h1>
+      <ul>
+        <li>
+          <a href="">Il css di questo file non vede il css dell'altro file</a>
+        </li>
+      </ul>
     </div>
     <h2><ListNames></ListNames></h2>
   </div>
