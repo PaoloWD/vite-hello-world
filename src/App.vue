72b375bd8dce880253f7279a02aedeea7a959c00
PaoloWD <paolo.lore.93@gmail.com>
2022-12-01 14:15:54 +0100

    complete component SayHello and print

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
-<script></script>
+<script>
+import SayHello from "./components/SayHello.vue";
+export default {
+  components: { SayHello },
+  data() {
+    return {};
+  },
+};
+</script>
 
-<template></template>
+<template>
+  <div class="container">
+    <SayHello></SayHello>
+  </div>
+</template>
 
 <style scoped></style>
